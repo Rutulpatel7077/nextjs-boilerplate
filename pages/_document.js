21302138c0012449a97fe6debe0009b1c0edd633
Rutul Patel <rutulpatel7077@gmail.com>
2022-10-11 01:01:17 -0300

    pwa

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ export default function Document() {
         <meta name="description" content="Best PWA App in the world" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
