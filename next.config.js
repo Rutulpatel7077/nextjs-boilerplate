b13a3b0228f9bac6955a26ed907429f8429c80cf
Rutul Patel <rutulpatel7077@gmail.com>
2022-10-11 00:42:29 -0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  mode: 'production',
+  // disable: process.env.NODE_ENV === 'development',
+})
+
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+})
 
 module.exports = nextConfig
